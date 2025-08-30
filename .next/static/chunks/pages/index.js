038dc33bb0c1dc03b83ceacfdb795fb9ba86e538
OryemaStephen <oryemasteve@gmail.com>
2025-08-30 14:02:20 +0300

    Integrated API for property detail

--- .next/static/chunks/pages/index.js
+++ .next/static/chunks/pages/index.js
@@ -1,8 +1,9 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/node_modules_next_f739d63f._.js",
+  "static/chunks/node_modules_next_dist_756b85fa._.js",
   "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_axios_lib_9aa2336a._.js",
   "static/chunks/node_modules_1638b3e7._.js",
-  "static/chunks/[root-of-the-server]__12799058._.js",
+  "static/chunks/[root-of-the-server]__685479d0._.js",
   "static/chunks/pages_index_5771e187._.js",
-  "static/chunks/pages_index_8003cedb._.js"
+  "static/chunks/pages_index_96611f0b._.js"
 ])
